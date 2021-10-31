db7ca7d614fceb11355a894e9a64813bfd7d1a6c
tateno <tateno-masa@gmail.com>
2021-10-31 15:48:25 +0900

    検索ワード入力欄と検索ボタン設置

--- app/src/main/java/com/example/searchgooglebooks/SearchTopActivity.kt
+++ app/src/main/java/com/example/searchgooglebooks/SearchTopActivity.kt
@@ -2,10 +2,20 @@ package com.example.searchgooglebooks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class SearchTopActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_top)
+
+        val inputText: EditText = findViewById(R.id.input_text)
+        val searchButton: Button = findViewById(R.id.search_button)
+        searchButton.setOnClickListener {
+            val query = inputText.text.toString()
+            Log.d("DEBUG_LOG", query)
+        }
     }
 }
