f07609f011c773063c76941695d422fa574fc7aa
tateno <tateno-masa@gmail.com>
2021-11-13 12:04:45 +0900

    Retrofit呼び出しをrepositoryに移動&不要になったクラスを削除

--- app/src/main/java/com/example/searchgooglebooks/data/repository/SearchBookRepository.kt
+++ app/src/main/java/com/example/searchgooglebooks/data/repository/SearchBookRepository.kt
@@ -2,15 +2,36 @@ package com.example.searchgooglebooks.data.repository
 
 import com.example.searchgooglebooks.data.model.GoogleBook
 import com.example.searchgooglebooks.api.SearchBookApi
-import com.example.searchgooglebooks.api.SearchBookProvider
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class SearchBookRepository {
 
-    private val provide: SearchBookProvider = SearchBookProvider()
+    private val baseUrl: String = "https://www.googleapis.com"
 
     suspend fun getGoogleBooks(query: String): Response<GoogleBook> {
-        val api = provide.retrofit.create(SearchBookApi::class.java)
-        return api.searchGoogleBook(query)
+        val moshi: Moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(client)
+            .build()
+
+        val service = retrofit.create(SearchBookApi::class.java)
+        return service.searchGoogleBook(query)
     }
 }
